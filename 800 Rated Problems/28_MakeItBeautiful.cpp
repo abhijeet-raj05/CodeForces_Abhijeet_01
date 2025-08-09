23a2397c70c69b6b54338de3d3bee2018abedcff
Abhijeet Raj <abhijeetoo505@gmail.com>
2025-08-09 16:42:34 +0530

    Add files via upload

---
+++ 800 Rated Problems/28_MakeItBeautiful.cpp
@@ -0,0 +1,50 @@
+#pragma GCC optimize("O3", "inline")
+#include <bits/stdc++.h>
+using namespace std;
+#define pb push_back
+using ll = long long;
+
+void solution()
+{
+    ll n;
+    cin >> n;
+    vector<ll> a(n);
+    for (int i = 0; i < n; i++){
+        cin >> a[i];
+    }
+
+    sort(a.begin(), a.end());
+    if (a.front() == a.back())
+    {
+        cout << "NO\n";
+        return;
+    }
+    cout << "YES\n";
+    cout << a.back() << " " << a.front() << " ";
+    for (int i = n - 2; i >= 1; i--)
+    {
+        cout << a[i] << " ";
+    }
+    cout << "\n";
+}
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solution();
+    }
+
+    return 0;
+}
