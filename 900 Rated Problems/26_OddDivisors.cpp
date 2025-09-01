c823011e63befd65b409a2db2a61f9c91b791920
Abhijeet Raj <abhijeetoo505@gmail.com>
2025-09-01 22:51:27 +0530

    Create 26_OddDivisors.cpp

---
+++ 900 Rated Problems/26_OddDivisors.cpp
@@ -0,0 +1,31 @@
+#pragma GCC optimize("O3", "inline")
+#include <bits/stdc++.h>
+using namespace std;
+#define pb push_back
+using ll = long long;
+
+void solution(){
+    ll n; cin >> n;
+    if ((n & (n - 1)) == 0) {
+        cout << "NO\n";
+    } else {
+        cout << "YES\n";
+    }
+}
+
+int main() {
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ll t; cin >> t;
+    while(t--){
+        solution();
+    }
+
+    return 0;
+}
